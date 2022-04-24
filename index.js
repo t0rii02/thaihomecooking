cee5e31750dd76f2a16c6044375179117b789cb8
t0rii02 <b4con.hamm@gmail.com>
2022-04-24 19:01:33 +0700

    restyled dishes, added switch functionality

---
+++ index.js
@@ -0,0 +1,11 @@
+let myNumberList = [1,2,3,4,5,6]
+
+console.log(myNumberList[1])
+
+for(let i= 0; i < 10; i++) {
+    console.log(myNumberList[i])
+}
+
+function addTwoNumbers(n1, n2) {
+    console.log(n1 + n2)
+}
